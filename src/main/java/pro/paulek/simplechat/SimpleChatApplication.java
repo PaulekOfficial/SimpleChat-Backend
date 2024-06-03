4252a6857893e722b3d3e88379f51619fdced1bd
PaulekOfficial <pawel.krzysztof.fortuna@outlook.com>
2024-06-03 22:54:43 +0200

    first commit

---
+++ src/main/java/pro/paulek/simplechat/SimpleChatApplication.java
@@ -0,0 +1,13 @@
+package pro.paulek.simplechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleChatApplication.class, args);
+    }
+
+}
